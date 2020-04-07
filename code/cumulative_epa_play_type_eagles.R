532d4606c2023d62a1099ec12ff688c0dee0cdbd
Ben Arancibia <bcarancibia@bens-mbp.lan>
2020-04-06 21:23:00 -0400

    Committing epa total

---
+++ code/cumulative_epa_play_type_eagles.R
@@ -0,0 +1,130 @@
+library(tidyverse)
+library(dplyr)
+library(na.tools)
+library(ggimage)
+library(nflscrapR)
+library(bannerCommenter)
+library(teamcolors)
+library(ggrepel)
+library(tictoc)
+library(ggthemes)
+library(ggthemr)
+
+#################################################################
+##         Cumulative EPA by play type for 2019 Eagles         ##
+#################################################################
+
+tic("data generation")
+pbp <- read_csv(url("https://github.com/ryurko/nflscrapR-data/raw/master/play_by_play_data/regular_season/reg_pbp_2019.csv"))
+toc()
+
+pbp_rp <- pbp %>% 
+  filter(!is_na(epa), play_type=="no_play" | play_type=="pass" | play_type=="run")
+
+pbp_rp <- pbp_rp %>%
+  mutate(
+    pass = if_else(str_detect(desc, "( pass)|(sacked)|(scramble)"), 1, 0),
+    rush = if_else(str_detect(desc, "(left end)|(left tackle)|(left guard)|(up the middle)|(right guard)|(right tackle)|(right end)") & pass == 0, 1, 0),
+    success = ifelse(epa>0, 1 , 0)
+  ) 
+
+pbp_rp %>% filter(play_type=="no_play") %>% select(pass, rush, desc)  %>% head
+pbp_rp <- pbp_rp %>% filter(pass==1 | rush==1)
+
+pbp_rp_eagles <- pbp_rp %>%
+  filter(posteam == "PHI") 
+
+pass_pbp_eagles <- pbp_rp_eagles %>%
+  filter(pass == 1)
+
+rush_pbp_eagles <- pbp_rp_eagles %>%
+  filter(rush == 1)
+
+
+pass_pbp_eagles$count <- seq(1:718)
+rush_pbp_eagles$count <- seq(1:426)
+
+
+pass_pbp_eagles_filter <- pass_pbp_eagles %>%
+  select(posteam, epa, count)
+
+rush_pbp_eagles_filter <- rush_pbp_eagles %>%
+  select(posteam, epa, count)
+
+pass_eagles_cumsum <- within(pass_pbp_eagles_filter, epa_sum <- cumsum(pass_pbp_eagles_filter$epa))
+rush_eagles_cumsum <- within(rush_pbp_eagles_filter, epa_sum <- cumsum(rush_pbp_eagles_filter$epa))
+
+
+eagles_epa_total <- pass_eagles_cumsum %>%
+  mutate(Type = "Pass") %>%
+  bind_rows(rush_eagles_cumsum %>%
+              mutate(Type = "Rush"))
+
+ggthemr("fresh")
+ggplot(eagles_epa_total, aes(x = count, y= epa_sum, color = Type)) +
+  geom_line(size = 1) +
+  geom_segment(x = 96, xend = 96, y = - 25, yend = 50, color = "red", linetype = 2) + 
+  geom_segment(x = 142, xend = 142, y = - 25, yend = 50, color = "red", linetype = 2) +
+  xlab("Number of Plays") +
+  ylab("Total EPA") +
+  labs(caption = "Data From @nflscrapR") +
+  labs(title = "Philadelphia Eagles: Rush vs Pass EPA for 2019 Season") +
+  labs(color = "Play Type") +
+  scale_colour_manual(values = c("forest green", "black"))
+
+
+
+
+
+
+
+ggplot() +
+  geom_line(data = rush_eagles_cumsum, aes(x = count, y = epa_sum, color = "Rush"), colour = "gray") + 
+  geom_line(data = pass_eagles_cumsum, aes(x=count, y=epa_sum, color = "Pass")) +
+
+  
+  
+  
+
+##################################################################
+##                          DO NOT USE                          ##
+##################################################################
+pbp_rp_eagles$count <- seq(1:1144)
+
+pbp_rp_eagles_filter <- pbp_rp_eagles %>%
+  select(posteam, pass, rush, epa, count)
+
+pbp_rp_eagles_cumsum <- within(pbp_rp_eagles_filter, epa_sum <- cumsum(pbp_rp_eagles_filter$epa))
+
+
+pass_pbp_eagles <- pbp_rp_eagles_cumsum %>%
+  filter(pass == 1)
+
+run_pbp_eagles <- pbp_rp_eagles_cumsum %>%
+  filter(rush == 1)
+
+ggplot(pbp_rp_eagles_cumsum, aes(x= count, y=epa_sum)) +
+  geom_line() 
+
+ggplot(pass_pbp_eagles, aes(x = count, y = epa_sum)) + 
+  geom_line()
+
+
+ggplot(run_pbp_eagles, aes(x = count, y = epa_sum)) + 
+  geom_line()
+
+ggplot(pass_eagles_cumsum, aes(x=count, y=epa_sum)) +
+  geom_line()
+
+ggplot(rush_eagles_cumsum, aes(x=count, y = epa_sum)) + 
+  geom_line()
+
+
+
+
+
+
+
+
+
+
--- code/examples_scrap.R
+++ code/examples_scrap.R
@@ -5,7 +5,7 @@ library(ggimage)
 library(nflscrapR)
 library(bannerCommenter)
 library(teamcolors)
-
+library(ggrepel)
 ###########################################################################
 ###########################################################################
 ###                                                                     ###
@@ -146,4 +146,156 @@ chart %>%
 
 pbp_rp %>% filter(play_type=="no_play") %>% 
   select(desc, pass, passer_player_name, rusher_player_name, receiver_player_name) %>% head()
+
+pbp_players <- pbp_rp %>% 
+  mutate(
+    passer_player_name = ifelse(play_type == "no_play" & pass == 1, 
+                                str_extract(desc, "(?<=\\s)[A-Z][a-z]*\\.\\s?[A-Z][A-z]+(\\s(I{2,3})|(IV))?(?=\\s((pass)|(sack)|(scramble)))"),
+                                passer_player_name),
+    receiver_player_name = ifelse(play_type == "no_play" & str_detect(desc, "pass"), 
+                                  str_extract(desc, 
+                                              "(?<=to\\s)[A-Z][a-z]*\\.\\s?[A-Z][A-z]+(\\s(I{2,3})|(IV))?"),
+                                  receiver_player_name),
+    rusher_player_name = ifelse(play_type == "no_play" & rush == 1, 
+                                str_extract(desc, "(?<=\\s)[A-Z][a-z]*\\.\\s?[A-Z][A-z]+(\\s(I{2,3})|(IV))?(?=\\s((left end)|(left tackle)|(left guard)|		(up the middle)|(right guard)|(right tackle)|(right end)))"),
+                                rusher_player_name)
+  )
+
+
+qbs <- pbp_players %>% 
+  mutate(
+    name = ifelse(!is_na(passer_player_name), passer_player_name, rusher_player_name),
+    rusher = rusher_player_name,
+    receiver = receiver_player_name,
+    play = 1
+  ) %>%
+  group_by(name, posteam) %>%
+  summarize (
+    n_dropbacks = sum(pass),
+    n_rush = sum(rush),
+    n_plays = sum(play),
+    epa_per_play = sum(epa)/n_plays,
+    success_per_play =sum(success)/n_plays
+  ) %>%
+  filter(n_dropbacks>=100)
+
+
+
+qbs %>%
+  ggplot(aes(x = success_per_play, y = epa_per_play)) +
+  geom_hline(yintercept = mean(qbs$epa_per_play), color = "red", linetype = "dashed") +
+  geom_vline(xintercept =  mean(qbs$success_per_play), color = "red", linetype = "dashed") +
+  geom_point(color = ifelse(qbs$posteam == "SF", "red", "black"), cex=qbs$n_plays/60, alpha=1/4) +
+  geom_text_repel(aes(label=name),
+                  force=1, point.padding=0,
+                  segment.size=0.1) +
+  labs(x = "Success rate",
+       y = "EPA per play",
+       caption = "Data from nflscrapR",
+       title = "QB success rate and EPA/play",
+       subtitle = "2018, min 100 pass attempts, includes all QB's rush and pass plays") +
+  theme_bw() +
+  theme(axis.title = element_text(size = 12),
+        axis.text = element_text(size = 10),
+        plot.title = element_text(size = 16, hjust = 0.5),
+        plot.subtitle = element_text(size = 14, hjust = 0.5),
+        plot.caption = element_text(size = 12))
+
+
+
+
+first <- 2009 #first season to grab. min available=2009
+last <- 2019 # most recent season
+
+datalist = list()
+for (yr in first:last) {
+  pbp <- read_csv(url(paste0("https://github.com/ryurko/nflscrapR-data/raw/master/play_by_play_data/regular_season/reg_pbp_", yr, ".csv")))
+  games <- read_csv(url(paste0("https://raw.githubusercontent.com/ryurko/nflscrapR-data/master/games_data/regular_season/reg_games_", yr, ".csv")))
+  pbp <- pbp %>% inner_join(games %>% distinct(game_id, week, season)) %>% select(-fumble_recovery_2_yards)
+  datalist[[yr]] <- pbp # add it to your list
+}
+
+#pbp_all <- dplyr::bind_rows(datalist) doesn't work
+
+pbp_all <- do.call(rbind.data.frame, datalist)
+
+pbp_all %>% group_by(home_team) %>%summarize(n=n(), seasons=n_distinct(season), minyr=min(season), maxyr=max(season)) %>% 
+  arrange(seasons)
+
+
+pbp_all <- pbp_all %>% 
+  mutate_at(vars(home_team, away_team, posteam, defteam), funs(case_when(
+    . %in% "JAX" ~ "JAC",
+    . %in% "STL" ~ "LA",
+    . %in% "SD" ~ "LAC",
+    TRUE ~ .
+  ))) 
+
+
+saveRDS(pbp_all, file="FILENAME.rds")
+pbp_all <- readRDS("FILENAME.rds")
+
+pbp_all_rp <- pbp_all %>%
+  filter(!is_na(epa), !is_na(posteam), play_type=="no_play" | play_type=="pass" | play_type=="run") %>%
+  mutate(
+    pass = if_else(str_detect(desc, "( pass)|(sacked)|(scramble)"), 1, 0),
+    rush = if_else(str_detect(desc, "(left end)|(left tackle)|(left guard)|(up the middle)|(right guard)|(right tackle)|(right end)") & pass == 0, 1, 0),
+    success = ifelse(epa>0, 1 , 0),
+    passer_player_name = ifelse(play_type == "no_play" & pass == 1, 
+                                str_extract(desc, "(?<=\\s)[A-Z][a-z]*\\.\\s?[A-Z][A-z]+(\\s(I{2,3})|(IV))?(?=\\s((pass)|(sack)|(scramble)))"),
+                                passer_player_name),
+    receiver_player_name = ifelse(play_type == "no_play" & str_detect(desc, "pass"), 
+                                  str_extract(desc, "(?<=to\\s)[A-Z][a-z]*\\.\\s?[A-Z][A-z]+(\\s(I{2,3})|(IV))?"),
+                                  receiver_player_name),
+    rusher_player_name = ifelse(play_type == "no_play" & rush == 1, 
+                                str_extract(desc, "(?<=\\s)[A-Z][a-z]*\\.\\s?[A-Z][A-z]+(\\s(I{2,3})|(IV))?(?=\\s((left end)|(left tackle)|(left guard)|		      (up the middle)|(right guard)|(right tackle)|(right end)))"),
+                                rusher_player_name),
+    name = ifelse(!is_na(passer_player_name), passer_player_name, rusher_player_name),
+    yards_gained=ifelse(play_type=="no_play",NA,yards_gained),
+    play=1
+  ) %>%
+  filter(pass==1 | rush==1)
+
+pbp_all_rp %>% filter(pass==1 & !is.na(passer_player_name))%>% mutate(
+  arod = if_else(posteam=="GB"&passer_player_name=="A.Rodgers",1,0),
+  early = if_else(season<=2014,1,0)
+) %>%
+  group_by(arod,early) %>%
+  summarize(mean_epa=mean(epa), ypp=mean(yards_gained, na.rm = TRUE)) %>% arrange(-early)
+
+
+
+
+##################################################################
+##       Lee Sharp - https://github.com/leesharpe/nfldata       ##
+##################################################################
+
+standings <- read_csv("http://www.habitatring.com/standings.csv")
+standings %>% head()
+
+standings %>% 
+  filter(playoff == "WonSB") %>% 
+  group_by(seed) %>% 
+  summarize(count=n())
+
+ggplot(standings,aes(x=scored,y=allowed)) +
+  theme_minimal() +
+  geom_point(aes(color=playoff)) +
+  xlab("Points Scored") +
+  ylab("Points Allowed") +
+  labs(title="Points Scored vs. Points Allowed")
+
+games <- read_csv("http://www.habitatring.com/games.csv")
+games %>% 
+  select(season,week,location,away_team,away_score,home_team,home_score,result) %>% 
+  head()
+
+home_games <- games %>% filter(location == "Home")
+summary(home_games$result)
+
+
+
+
+
+
 
